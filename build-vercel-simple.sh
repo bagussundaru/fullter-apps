fc7e2da0ce47bb97f1e7efa9f74dcddd4b3e9147
bagussundaru <bagus.sundaru@.com>
2025-08-04 11:50:59 +0700

    Fix build script: replace wget with curl for Flutter download

--- build-vercel-simple.sh
+++ build-vercel-simple.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "🚀 Installing Flutter..."
-wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz
-tar xf flutter_linux_3.24.5-stable.tar.xz
+curl -o flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz
+tar xf flutter.tar.xz
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 echo "🔧 Setting up Flutter..."
